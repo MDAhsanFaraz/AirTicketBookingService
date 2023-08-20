9249e6ba3c9140c5c40c77d29d630c1737461aa0
Faraz <faraz.com>
2023-08-20 16:05:35 +0530

    Completed create Booking

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
 
-const {PORT}=require('./config/serverConfig');
+const {PORT,FLIGHT_SERVICE_PATH}=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 const db=require('./models/index');
 
@@ -11,7 +11,7 @@ const setupAndStartServer = ()=> {
           app.use(bodyParser.json());
           app.use(bodyParser.urlencoded({extended:true}));
 
-          app.use('./api',apiRoutes);
+          app.use('/api',apiRoutes);
 
           app.listen(PORT,()=>{
                     console.log(`Server started on port ${PORT}`);
